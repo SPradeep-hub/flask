adca77bc52bf2849976efcd51f4777b29f955486
pradeep <pradeeps22092005@gmail.com>
2026-03-20 01:38:57 +0530

    backend

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from processing.vidToImage import process_video
 app = Flask(__name__, 
             template_folder="app/templates",
             static_folder="app/static",
-            UPLOAD_FOLDER = "uploads",
-            OUTPUT_FOLDER = "output_folder")
+)
 
 # Home page
 @app.route("/")
@@ -16,6 +15,10 @@ def home():
     return render_template("index.html")
 
 # Login page
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/login")
 def login():
     return render_template("login.html")
@@ -29,23 +32,11 @@ def auth():
 @app.route("/how-it-works")
 def how():
     return render_template("howitworks.html")
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-@app.route("/", methods=["GET", "POST"])
-def upload():
-    if request.method == "POST":
-        file = request.files["video"]
 
-        video_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(video_path)
 
-        output_path = os.path.join(OUTPUT_FOLDER, file.filename.split('.')[0])
-
-        process_video(video_path, output_path)
-
-        return "Frames extracted successfully!"
-
+@app.route("/index")
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
